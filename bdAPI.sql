57304482dba142fcd7a1b8907716aa025f0fa889
Sebastian Villalobos <dereklevilla45@gmail.com>
2025-05-23 08:51:00 -0600

    Add initial database schema for bdAPI
    Introduces the initial SQL schema for the bdAPI database. It includes the creation of tables for User, Products, Compra, and CompraProducto with their respective fields, primary keys, and foreign key relationships. Also includes a safeguard to create the database if it does not exist.

---
+++ bdAPI.sql
@@ -0,0 +1,48 @@
+-- DANGER 
+DROP DATABASE bdAPI;
+
+-- INIT
+CREATE DATABASE IF NOT EXISTS bdAPI;
+
+--------- Tables --------
+CREATE TABLE IF NOT EXISTS bdAPI.User (
+    --
+    userId int NOT NULL AUTO_INCREMENT,
+    nombre varchar(50) NOT NULL,
+    apellidos varchar(50) NOT NULL,
+    edad int,
+    administrador bit,
+    estado bit NOT NULL,
+    --
+    PRIMARY KEY(userId)
+);
+
+CREATE TABLE IF NOT EXISTS bdAPI.Products (
+    --
+    productId int NOT NULL AUTO_INCREMENT,
+    nombre varchar(50) NOT NULL,
+    precio float NOT NULL,
+    estado bit NOT NULL,
+    --
+    PRIMARY KEY(productId)
+);
+
+CREATE TABLE IF NOT EXISTS bdAPI.Compra (
+    --
+    CompraId int NOT NULL AUTO_INCREMENT,
+    userId int NOT NULL AUTO_INCREMENT,
+    --
+    PRIMARY KEY(CompraId),
+    FOREIGN KEY (userId) REFERENCES User(userId)
+);
+
+CREATE TABLE IF NOT EXISTS bdAPI.CompraProducto(
+    --
+    CompraProductoId int NOT NULL AUTO_INCREMENT,
+    CompraId int NOT NULL AUTO_INCREMENT,
+    productId int NOT AUTO_INCREMENT,
+    --
+    PRIMARY KEY(CompraProductoId),
+    FOREIGN KEY(CompraId) REFERENCES Compra(CompraId),
+    FOREIGN KEY(productId) REFERENCES Products(productId)
+);
