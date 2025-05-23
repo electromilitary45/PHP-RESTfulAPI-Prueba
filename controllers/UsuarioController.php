d0d5f407c2636f9a7330d71d76d6f2b0ad942f60
Sebastian Villalobos <dereklevilla45@gmail.com>
2025-05-23 16:41:09 -0600

    Update UsuarioController.php

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -39,38 +39,4 @@ class UsuarioController{
         }
     }
 }
-// require_once '../db/dbase.php';
-// require_once '../models/Usuario.php';
-
-// $database = new Database();
-// $db = $database->getConnection();
-
-// $usuario = new Usuario($db);
-
-// $method = $_SERVER['REQUEST_METHOD'];
-
-// switch($method) {
-//     case 'GET':
-//         break;
-//     case 'POST':
-//         $data = json_decode(file_get_contents("php://input"));
-//         $usuario->nombre = $data->nombre;
-//         $usuario->apellidos = $data->apellidos;
-//         $usuario->edad = $data->edad;
-//         $usuario->administrador = $data->administrador;
-
-//         if($usuario->CrearUsuario()){
-//             http_response_code(201);
-//             echo json_encode(array("mensaje" => "Usuario creado correctamente."));
-//         }else{
-//             http_response_code(503);
-//             echo json_encode(array("mensaje" => "No se pudo crear el usuario."));
-//         }
-//         break;
-
-//     default:
-//         http_response_code(405);
-//         echo json_encode(array("mensaje"=>"Metodo no permitido."));
-//         break;
-// }
 ?>
